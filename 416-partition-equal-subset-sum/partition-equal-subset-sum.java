650935c83d922c24a16776cf2cf3c7802ba1899a
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-04-17 16:34:21 +0530

    Time: 70 ms (32.51%) | Memory: 84.7 MB (7.48%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -10,7 +10,29 @@ class Solution {
         }
         target /= 2;
         dp = new Boolean[nums.length][target + 1];
-        return solve(nums.length - 1, target, nums);
+        for(int j = 0; j < target + 1; j++){
+            if(j == nums[0]){
+                dp[0][j] = true;
+            }else{
+                dp[0][j] = false;
+            }
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            dp[i][0] = true;
+        }
+
+        for(int i = 1; i < nums.length; i++){
+            for(int j = 1; j < target + 1; j++){
+                boolean notTake = dp[i - 1][j];
+                boolean take = false;
+                if(j >= nums[i]){
+                    take = dp[i - 1][j - nums[i]];
+                }
+                dp[i][j] = take || notTake;
+            }
+        }
+        return dp[nums.length - 1][target];
     }
 
     public boolean solve(int idx, int target, int[] nums){
