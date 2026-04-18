291ee913204008a4303d1673febeecdc347d3e9d
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-04-18 12:38:06 +0530

    Time: 1 ms (99.56%) | Memory: 47.2 MB (60.22%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,35 +1,19 @@
 class Solution {
-    static int[] dp;
     public int jump(int[] nums) {
         int n = nums.length;
-        dp = new int[n];
-        dp[n - 1] = 0;
-        for(int i = n - 2; i >= 0; i--){
-            int minCost = (int)1e9;
-            for(int k = nums[i]; k >= 1; k--){
-                if(i + k < n){
-                    minCost = Math.min(minCost, 1 + dp[i + k]);
-                }
-            }
-            dp[i] = minCost;
-        }
-        return dp[0];
-    }
+        int left = 0;
+        int right = 0;
+        int maxReach = 0;
+        int jumps = 0;
 
-    public int solve(int idx, int[] nums){
-        if(idx == nums.length - 1){
-            return 0;
+        while(right < n - 1){
+            jumps++;
+            for(int i = left; i <= right; i++){
+                maxReach = Math.max(maxReach, nums[i] + i);
+            }
+            left = right + 1;
+            right = maxReach;
         }
-        if(idx >= nums.length){
-            return (int)1e9;
-        }
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        int minCost = (int)1e9;
-        for(int i = nums[idx]; i >= 1; i--){
-            minCost = Math.min(minCost, 1 + solve(idx + i, nums));
-        }
-        return dp[idx] =  minCost;
+        return jumps;
     }
 }
