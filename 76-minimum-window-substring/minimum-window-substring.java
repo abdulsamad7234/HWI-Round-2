327d4ec552e490a5f7c50cded191027e3f8decba
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-04-30 00:33:30 +0530

    Time: 102 ms (13.66%) | Memory: 46.5 MB (63.79%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int[] arrS = new int[256];
+        int[] arrT = new int[256];
+        for(int i = 0; i < t.length(); i++){
+            char ch = t.charAt(i);
+            arrT[ch]++;
+        }
+
+        int minL = Integer.MAX_VALUE;
+        int startIdx = -1;
+        int left = 0;
+        for(int right = 0; right < s.length(); right++){
+            char ch = s.charAt(right);
+            arrS[ch]++;
+            if(isContains(arrS, arrT)){
+                if(minL > (right - left + 1)){
+                    minL = right - left + 1;
+                    startIdx = left;
+                }
+            }
+            while(isContains(arrS, arrT)){
+                arrS[s.charAt(left)]--;
+                left++;
+                if(isContains(arrS, arrT)){
+                    if(minL > (right - left + 1)){
+                        minL = right - left + 1;
+                        startIdx = left;
+                    }
+                }
+            }
+        }
+        return (minL == Integer.MAX_VALUE) ? "" : s.substring(startIdx, startIdx + minL);
+
+    }
+
+    boolean isContains(int[] arrS, int[] arrT){
+        for(int i = 0; i < 256; i++){
+            if(arrT[i] > arrS[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
