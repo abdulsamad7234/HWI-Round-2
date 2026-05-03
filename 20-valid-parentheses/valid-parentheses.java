5af4bf644a4218a0caf429b4571757a90ad06c8a
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-05-03 12:27:03 +0530

    Time: 3 ms (87.70%) | Memory: 43.5 MB (25.97%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -4,17 +4,20 @@ class Solution {
         if((s.length() & 1) == 1) return false;
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
-            if(st.isEmpty() && (ch == ')' || ch == ']' || ch == '}')) return false;
             if(ch == '(' || ch == '{' || ch == '['){
                 st.push(ch);
-            }else if(ch == ')' && st.peek() == '('){
-                st.pop();
-            }else if(ch == ']' && st.peek() == '['){
-                st.pop();
-            }else if(ch == '}' && st.peek() == '{'){
-                st.pop();
             }else{
-                st.push(ch);
+                if(st.isEmpty()) return false;
+                char top = st.peek();
+                if(ch == ')' && top == '('){
+                    st.pop();
+                }else if(ch == '}' && top == '{'){
+                    st.pop();
+                }else if(ch == ']' && top == '['){
+                    st.pop();
+                }else{
+                    return false;
+                }
             }
         }
 
