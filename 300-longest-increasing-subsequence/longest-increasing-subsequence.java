ed23c337dd3416df594bc98732115babb956dafc
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-05-12 20:02:45 +0530

    Time: 48 ms (34.34%) | Memory: 46.3 MB (40.71%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,31 +1,17 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        List<Integer> list = new ArrayList<>();
+        int dp[] = new int[n];
+        Arrays.fill(dp, 1);
+        int max = 1;
         for(int i = 0; i < n; i++){
-            int idx = findLower(nums[i], list);
-            if(idx == list.size()){
-                list.add(nums[i]);
-            }else{
-                list.set(idx, nums[i]);
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i] && dp[j] + 1 > dp[i]){
+                    dp[i] = dp[j] + 1;
+                    max = Math.max(max, dp[i]);
+                }
             }
         }
-        return list.size();
-    }
-
-    public int findLower(int target, List<Integer> list){
-        int ans = list.size();
-        int low = 0;
-        int high = list.size() - 1;
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(list.get(mid) >= target){
-                ans = mid;
-                high = mid - 1;
-            }else{
-                low = mid + 1;
-            }
-        }
-        return ans;
+        return max;
     }
 }
