c957316a06304a72278c312f82a4259ddc56da70
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-06-02 14:51:39 +0530

    Time: 481 ms (61.86%) | Memory: 0B (100.00%) - LeetSync

---
+++ 608-tree-node/tree-node.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+with recursive cte as(select id, p_id from Tree where p_id is null
+union all
+select t.id,t.p_id from tree t join cte  c on t.p_id=c.id) select id ,case when p_id is null then"Root" when id in (select p_id from tree where p_id is not null) then "Inner" else "Leaf" end as type from cte order by id;
