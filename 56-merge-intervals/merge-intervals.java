8815fb73ec1f671e372f11dc5f4eb4e4070e6069
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-06-03 21:16:03 +0530

    Time: 8 ms (90.45%) | Memory: 48.9 MB (70.80%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,31 +1,32 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        List<List<Integer>> ansList = new ArrayList<>();
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        List<int[]> ans = new ArrayList<>();
-        if(intervals.length <= 1){
-            return intervals;
-        }
-        int lb = intervals[0][0];
-        int ub = intervals[0][1];
+        int start = intervals[0][0];
+        int end = intervals[0][1];
         for(int i = 1; i < intervals.length; i++){
-            int lb_curr = intervals[i][0];
-            int ub_curr = intervals[i][1];
-            if(lb_curr <= ub){
-                ub = Math.max(ub, ub_curr);
+            int st = intervals[i][0];
+            int en = intervals[i][1];
+            if(st <= end){
+                end = Math.max(end, en);
             }else{
-                ans.add(new int[]{lb, ub});
-                lb = lb_curr;
-                ub = ub_curr;
-            }
-            if(i == intervals.length - 1){
-                ans.add(new int[]{lb, ub});
+                List<Integer> list = new ArrayList<>();
+                list.add(start);
+                list.add(end);
+                ansList.add(list);
+                start = st;
+                end = en;
             }
         }
-        int final_ans[][] = new int[ans.size()][2];
-        int idx = 0;
-        for(int[] x : ans){
-            final_ans[idx++] = x;
+        List<Integer> list = new ArrayList<>();
+        list.add(start);
+        list.add(end);
+        ansList.add(list);
+        int[][] ans = new int[ansList.size()][2];
+        for(int i = 0; i < ansList.size(); i++){
+            ans[i][0] = ansList.get(i).get(0);
+            ans[i][1] = ansList.get(i).get(1);
         }
-        return final_ans;
+        return ans;
     }
 }
