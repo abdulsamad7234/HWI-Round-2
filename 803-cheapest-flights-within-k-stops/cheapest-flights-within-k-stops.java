db47df5127bf3bded883a77a0bf4d9b8eb044ea5
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-06-15 12:40:41 +0530

    Time: 6 ms (85.40%) | Memory: 46.4 MB (82.18%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,23 +1,48 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        List<List<Pair>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] flight : flights){
+            int u = flight[0];
+            int v = flight[1];
+            int cost = flight[2];
+            adj.get(u).add(new Pair(v, cost));
+        }
+
         int[] dist = new int[n];
         Arrays.fill(dist, Integer.MAX_VALUE);
+        Queue<int[]> q = new LinkedList<>();
         dist[src] = 0;
-        for(int i = 0; i <= k; i++){
-            int[] temp = dist.clone();
-            for(int j = 0; j < flights.length; j++){
-                int u = flights[j][0];
-                int v = flights[j][1];
-                int p = flights[j][2];
-                if(dist[u] != Integer.MAX_VALUE && dist[u] + p < temp[v]){
-                    temp[v] = dist[u] + p;
+        q.add(new int[]{src, 0, 0});
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int u = curr[0];
+            int cost = curr[1];
+            int stops = curr[2];
+            if(stops == k + 1) continue;
+            for(Pair adjPair : adj.get(u)){
+                int v = adjPair.node;
+                int wt = adjPair.dist;
+                if(cost + wt < dist[v]){
+                    dist[v] = cost + wt;
+                    q.add(new int[]{v, dist[v], stops + 1});
                 }
             }
-            dist = temp;
         }
         if(dist[dst] == Integer.MAX_VALUE){
             return -1;
         }
         return dist[dst];
+    }
+
+    class Pair{
+        int node;
+        int dist;
+        public Pair(int node, int dist){
+            this.node = node;
+            this.dist = dist;
+        }
     }
 }
