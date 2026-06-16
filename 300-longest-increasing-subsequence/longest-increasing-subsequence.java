25207ab3305c49ee3c0886c50601b98784a7e705
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-06-16 16:04:12 +0530

    Time: 373 ms (12.06%) | Memory: 236.9 MB (17.33%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,32 +1,25 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        List<Integer> list = new ArrayList<>();
-        list.add(nums[0]);
-        for(int i = 1; i < n; i++){
-            int idx = findIndex(nums[i], list);
-            if(idx == list.size()){
-                list.add(nums[i]);
-            }else{
-                list.set(idx, nums[i]);
-            }
+        int dp[][] = new int[nums.length][nums.length + 1];
+        for(int i = 0; i < nums.length; i++){
+            Arrays.fill(dp[i], -1);
         }
-        return list.size();
+        return solve(nums.length - 1, -1, nums, dp);
     }
 
-    public int findIndex(int target, List<Integer> list){
-        int ans = list.size();
-        int low = 0;
-        int high = list.size() - 1;
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(list.get(mid) >= target){
-                ans = mid;
-                high = mid - 1;
-            }else{
-                low = mid + 1;
-            }
+    int solve(int idx, int prev, int[] nums, int[][] dp){
+        if(idx < 0){
+            return 0;
         }
-        return ans;
+        if(dp[idx][prev + 1] != -1){
+            return dp[idx][prev + 1];
+        }
+        if(prev == -1 || nums[idx] < nums[prev]){
+            int take = 1 + solve(idx - 1, idx, nums, dp);
+            int notTake = solve(idx - 1, prev, nums, dp);
+            return dp[idx][prev + 1] = Math.max(take, notTake);
+        }
+        int notTake = solve(idx - 1, prev, nums, dp);
+        return dp[idx][prev + 1] = notTake;
     }
 }
