4a9e1b54d0aa769c773e819559f06d846a7e23f3
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-06-17 10:15:28 +0530

    Time: 5 ms (88.02%) | Memory: 46.2 MB (41.65%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,16 +1,31 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int dp[] = new int[nums.length];
-        Arrays.fill(dp, 1);
-        int maxL = 1;
-        for(int i = 1; i < nums.length; i++){
-            for(int prev = 0; prev < i; prev++){
-                if(nums[prev] < nums[i] && dp[prev] + 1 > dp[i]){
-                    dp[i] = dp[prev] + 1;
-                    maxL = Math.max(maxL, dp[i]);
-                }
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            int idx = upperBound(nums[i], ans);
+            if(idx == ans.size()){
+                ans.add(nums[i]);
+            }else{
+                ans.set(idx, nums[i]);
             }
         }
-        return maxL;
+        return ans.size();
+    }
+
+    int upperBound(int num, ArrayList<Integer> list){
+        int ans = -1;
+        int l = 0;
+        int r = list.size() - 1;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(list.get(mid) >= num){
+                ans = mid;
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+
+        return (ans == -1) ? list.size() : ans;
     }
 }
