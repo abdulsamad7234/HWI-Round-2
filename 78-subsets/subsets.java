3241797579fa6187819386a86987f31df9e4b78e
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-06-18 01:23:38 +0530

    Time: 1 ms (88.83%) | Memory: 43.9 MB (93.95%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        solve(0, nums, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    void solve(int idx, int[] nums, List<Integer> list, List<List<Integer>> ans){
+        if(idx == nums.length){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        //not Take
+        solve(idx + 1, nums, list, ans);
+        //Take
+        list.add(nums[idx]);
+        solve(idx + 1, nums, list, ans);
+        list.remove(list.size() - 1);
+    }
+}
