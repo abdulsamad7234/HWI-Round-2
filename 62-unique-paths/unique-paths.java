764b03142938fe0e95e1506fe1635289eb61e48c
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-07-01 15:25:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (81.70%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,33 +1,19 @@
 class Solution {
-    static Integer[][] dp;
     public int uniquePaths(int m, int n) {
-        dp = new Integer[m][n];
+        int[] prev = new int[n];
         for(int i = 0; i < m; i++){
+            int[] curr = new int[n];
             for(int j = 0; j < n; j++){
                 if(i == 0 && j == 0){
-                    dp[i][j] = 1;
+                    curr[j] = 1;
                 }else{
-                    int up = (i > 0) ? dp[i - 1][j] : 0;
-                    int left = (j > 0) ? dp[i][j - 1] : 0;
-                    dp[i][j] = up + left;
+                    int up = (i > 0) ? prev[j] : 0;
+                    int left = (j > 0) ? curr[j - 1] : 0;
+                    curr[j] = up + left;
                 }
             }
+            prev = curr;
         }
-        return dp[m - 1][n - 1];
-    }
-
-    int solve(int i, int j, int m, int n){
-        if(i < 0 || i >= m || j < 0 || j >= n){
-            return 0;
-        }
-        if(i == m - 1 && j == n - 1){
-            return 1;
-        }
-        if(dp[i][j] != null){
-            return dp[i][j];
-        }
-        int right = solve(i, j + 1, m, n);
-        int down = solve(i + 1, j, m, n);
-        return dp[i][j] = right + down;
+        return prev[n - 1];
     }
 }
