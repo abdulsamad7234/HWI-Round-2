73b0436593a71d726105274353670b4c2e1a6ead
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-07-03 03:32:18 +0530

    Time: 2 ms (43.26%) | Memory: 47.4 MB (43.28%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,42 +1,49 @@
 class Solution {
+    static int[] size;
+    static int[] parent;
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        List<List<Integer>> adj = new ArrayList<>();
-        boolean vis[] = new boolean[n];
-        for(int i = 0; i < n + 1; i++){
-            adj.add(new ArrayList<>());
+        size = new int[n];
+        parent = new int[n];
+        for(int i = 0; i < n; i++){
+            size[i] = 1;
+            parent[i] = i;
         }
+        
+        int provinces = n;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                if(i == j) continue;
-                if(isConnected[i][j] == 1){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
+                if(i != j && isConnected[i][j] == 1){
+                    if(union(i, j) == true){
+                        provinces--;
+                    }
                 }
             }
         }
-        int count = 0;
-        for(int i = 0; i < n; i++){
-            if(vis[i] == false){
-                bfs(i, adj, vis);
-                count++;
-            }
-        }
-        return count;
+        return provinces;
     }
 
-    void bfs(int src, List<List<Integer>> adj, boolean vis[]){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(src);
-        vis[src] = true;
-        while(!q.isEmpty()){
-            int node = q.poll();
-            for(int adjNode : adj.get(node)){
-                if(!vis[adjNode]){
-                    vis[adjNode] = true;
-                    q.add(adjNode);
-                }
+    boolean union(int a, int b){
+        int parent_a = find(a);
+        int parent_b = find(b);
+        if(parent_a != parent_b){
+            if(size[parent_a] > size[parent_b]){
+                parent[parent_b] = parent_a;
+                size[parent_a] += size[parent_b];
+            }else{
+                parent[parent_a] = parent_b;
+                size[parent_b] += size[parent_a];
             }
+            return true;
+        }else{
+            return false;
         }
+    }
+
+    int find(int x){
+        if(parent[x] == x){
+            return x;
+        }
+        return parent[x] = find(parent[x]);
     }
 }
