2fca4159cbe11cc61ce21a9fc4c2146134878a76
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-08-09 10:43:47 +0530

    Time: 13 ms (3.19%) | Memory: 47 MB (5.19%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numDecodings(String s) {
+        Set<String> set = new HashSet<>();
+        for(int i = 1; i <= 26; i++){
+            set.add(String.valueOf(i));
+        }
+        int dp[] = new int[s.length()];
+        Arrays.fill(dp, -1);
+        return solve(0, s, set, dp);
+    }
+
+    int solve(int idx, String s, Set<String> set, int[] dp){
+        if(idx >= s.length()){
+            return 1;
+        }
+        if(dp[idx] != -1) return dp[idx];
+        int ways = 0;
+        for(int j = idx + 1; j <= s.length(); j++){
+            String sub = s.substring(idx, j);
+            if(set.contains(sub)){
+                ways += solve(j, s, set, dp);
+            }
+        }
+        return dp[idx] = ways;
+    }
+}
