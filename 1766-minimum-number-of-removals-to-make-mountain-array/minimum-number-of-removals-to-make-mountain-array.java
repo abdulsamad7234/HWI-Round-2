4c33f52667e026c662ac4d11fc4465e8794976f2
abdulsamad7234 <abdulsamadkhan4737@gmail.com>
2026-09-15 17:44:31 +0530

    Time: 39 ms (52.60%) | Memory: 46.7 MB (7.94%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.java
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.java
@@ -1,70 +1,39 @@
 class Solution {
     public int minimumMountainRemovals(int[] nums) {
         int n = nums.length;
-        List<Integer> left_len_LIS = new ArrayList<>();
-        List<Integer> right_len_LIS = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            left_len_LIS.add(1);
-            right_len_LIS.add(1);
-        }
-        findLIS(nums, left_len_LIS);
-        reverse(nums);
-        findLIS(nums, right_len_LIS);
-        Collections.reverse(right_len_LIS);
-        reverse(nums);
 
-        int len = 0;
-        
-        for(int i = 1; i < n - 1; i++){
-            if(left_len_LIS.get(i) > 1 && right_len_LIS.get(i) > 1){
-                len = Math.max(len, left_len_LIS.get(i) + right_len_LIS.get(i) - 1);
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            left[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    left[i] = Math.max(left[i], left[j] + 1);
+                }
             }
         }
 
-        return n - len;
-    }
+        for (int i = n - 1; i >= 0; i--) {
+            right[i] = 1;
 
-    List<Integer> findLIS(int[] nums, List<Integer> len_LIS){
-        int n = nums.length;
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            int curr = nums[i];
-            int idx = findLowerBound(curr, list);
-            if(idx == list.size()){
-                list.add(curr);
-            }else{
-                list.set(idx, curr);
-            }
-            len_LIS.set(i, idx + 1);
-        }
-        return len_LIS;
-    }
-
-    int findLowerBound(int ele, List<Integer> list){
-        int l = 0;
-        int r = list.size() - 1;
-        int ans = -1;
-        while(l <= r){
-            int mid = l + (r - l) / 2;
-            if(list.get(mid) >= ele){
-                ans = mid;
-                r = mid - 1;
-            }else{
-                l = mid + 1;
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] < nums[i]) {
+                    right[i] = Math.max(right[i], right[j] + 1);
+                }
             }
         }
-        return ans == -1 ? list.size() : ans;
-    }
 
-    void reverse(int[] nums){
-        int l = 0;
-        int r = nums.length - 1;
-        while(l < r){
-            int temp = nums[r];
-            nums[r] = nums[l];
-            nums[l] = temp;
-            l++;
-            r--;
+        int longestMountain = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            if (left[i] > 1 && right[i] > 1) {
+                int mountainLength = left[i] + right[i] - 1;
+                longestMountain = Math.max(longestMountain, mountainLength);
+            }
         }
+
+        return n - longestMountain;
     }
 }
